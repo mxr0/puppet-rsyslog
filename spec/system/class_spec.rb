b1468b9eeea1bf0574093beeee039e8c88483f77
Igor Galić <i.galic@brainsware.org>
2013-09-24 23:31:33 +0200

    rspec:system test covering install & service
    this rspec test checks that rsyslog is installed and the service
    is running - which is the functionality we have implemented so far.

---
+++ spec/system/class_spec.rb
@@ -0,0 +1,47 @@
+#   Copyright 2013 Brainsware
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+require 'spec_helper_system'
+
+describe 'rsyslog class' do
+    package_name = 'rsyslog'
+    service_name = 'rsyslog'
+    virtinst_package = 'python-virtinst'
+
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should work with no errors' do
+      pp = <<-EOS
+      class { 'rsyslog': }
+      EOS
+
+      # Run it twice and test for idempotency
+      puppet_apply(pp) do |r|
+        r.exit_code.should_not == 1
+        r.refresh
+        r.exit_code.should be_zero
+      end
+    end
+
+    describe package(package_name) do
+      it { should be_installed }
+    end
+    describe service(service_name) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+
+  end
+
+end
